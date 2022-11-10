e855e8e2c2446adb0967587d8b3a2411dff25fa7
pro100proger <nazar4075066@gmail.com>
2022-11-10 16:03:20 +0200

    add logic to login and registration pages

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,84 @@
 import "./login.style.css";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import PasswordInput from "../../components/PasswordInput/PasswordInput";
-import { ReactComponent as Arrow } from "../../icons/Arrow.svg";
+import {ReactComponent as Arrow} from "../../icons/Arrow.svg";
 import {ReactComponent as Calculator} from "../../icons/Calculator.svg";
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const login = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    });
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [errors, setErrors] = useState("");
+
+    const handleChange = ({target: {name, value}}) => {
+        setUser({...user, [name]: value});
+        setErrors({...errors, [name]: ''});
+    };
+
+    function loginUser(user) {
+        console.log("loginUser");
+        const sendUser = {
+            email: user.email,
+            password: user.password
+        };
+        axios.post("https://ujp-sports-hub.herokuapp.com/api/v1/password", sendUser, {})
+            .then((response) => {
+                localStorage.setItem('user', JSON.stringify(response.data))
+                navigate("/");
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log(error.response);
+                    console.log("error.response.status: ", error.response.status);
+                }
+            });
+    }
+
+    const handleClick = event => {
+        event.preventDefault()
+        if (isValid()) {
+            loginUser(user)
+        } else {
+            console.log(errors);
+        }
+    }
+
+    const validateInput = data => {
+        let errors = {}
+        if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            .test(data.email)) {
+            errors.email = "Please enter valid email"
+        }
+        if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(data.password)) {
+            errors.password = "Password must contain at least 8 characters (letters and numbers)"
+        }
+        return {
+            errors,
+            isValid: JSON.stringify(errors) === '{}'
+        }
+    }
+    const isValid = () => {
+        const {errors, isValid} = validateInput(user)
+        if (!isValid) {
+            setErrors(errors)
+        }
+        return isValid
+    }
+
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const navigate = useNavigate();
+    function registration() {
+        navigate("/registration");
+    }
+
+
     return (
         <div className={"login-page"}>
             <div className={"left-photo"}>
@@ -33,30 +107,36 @@ const login = () => {
                         Log in to your account to continue.
                     </div>
                     <form className="form-container">
+                        {errors.email && <p className='input_error'>
+                            {errors.email}
+                        </p>}
                         <CustomInput
                             type="text"
                             label={"Email"}
                             name={"email"}
                             placeholder={"Enter email"}
-                            // value={article.caption}
-                            // handleChange={handleChange}
+                            value={user.email}
+                            handleChange={handleChange}
                         />
+                        {errors.password && <p className='input_error'>
+                            {errors.password}
+                        </p>}
                         <PasswordInput
                             label={"Password"}
                             name={"password"}
                             placeholder={"Enter password"}
-                            // value={password.confirmPassword}
-                            // handleChange={handleChange}
+                            value={user.password}
+                            handleChange={handleChange}
                         />
                     </form>
 
                     <div className={"auxiliary-button-div"}>
-                        <button className={"button-for-link"}>
+                        <button className={"button-for-link"} onClick={registration}>
                             Don't have account yet
                         </button>
                     </div>
 
-                    <button className={"login-button-sign-in"}>
+                    <button className={"login-button-sign-in"} onClick={handleClick}>
                         Sign in
                     </button>
 
